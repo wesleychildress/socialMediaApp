1b0e7c0dab75d90efe29afcb0656b9cd686a73f3
WesleyChildress <wesleychildress@icloud.com>
2020-02-16 11:13:00 -0500

    update port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.engine('handlebars', exphbs({
 
 app.set('view engine', 'handlebars');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
     res.send('Welcome to Social Media App');
